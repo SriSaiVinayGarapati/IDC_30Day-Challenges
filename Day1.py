f99838f27607cd9188f0d5e63bda9a0afe7c9297
Vinay Garapati <vinaygarapati05@gmail.com>
2025-05-30 08:04:31 +0530

    Day1_2

---
+++ Day1.py
@@ -0,0 +1,1 @@
+print("Hello, World!...Sri Sai Vinay G welcome to IDC 30 day Challenge...Let's Rock")
---
+++ Day2.py
@@ -0,0 +1,7 @@
+#Program to calculate area of a rectangle
+
+length = int(input("Enter the length of a rectangle (in cm) : "))
+breadth  = int(input("Enter the breadth of a rectangle (in cm) : "))
+Area = length * breadth
+
+print(f'The Area of rectangle with (length : {length}cm and breadth : {breadth}cm) is : {Area}cm^2')
